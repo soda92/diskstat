79e168376b05143c28589577fbf7c0994c25a4fd
Maple <wang-jiaxun@outlook.com>
2024-10-24 15:06:23 +0800

    add console version, init gui version

--- disks.py
+++ disks.py
@@ -1,5 +1,9 @@
 import shutil
 from pathlib import Path
+import colorama
+
+from colorama import init
+init(autoreset=True)
 
 disk = 'A'
 disks = []
@@ -9,5 +13,16 @@ for i in range(26):
     if Path(path).exists():
         disks.append(path)
 
-# shutil.disk_usage
-print(disks)
+
+for d in disks:
+    usage = shutil.disk_usage(d)
+    print(d, end= ' ')
+    percent = usage.used / usage.total * 10
+    if percent > 9:
+        print(colorama.Fore.RED)
+    percent = int(percent)
+    print('[' + '=' * percent + ' '* (10-percent) + "]", end= " ")
+
+    free_gigabytes = usage.free/1024 /1024/1024
+    percent = usage.free / usage.total * 100
+    print(colorama.Fore.GREEN + f"free: {free_gigabytes:.1f} G ({percent:.1f}%)")
--- main.py
+++ main.py
@@ -1,9 +1,41 @@
 from PySide6 import QtWidgets
+import win32api
+
+
+def get_disk_name(path):
+    return win32api.GetVolumeInformation(f"{path}:\\")[0]
+
+
+class Disk(QtWidgets.QWidget):
+    def __init__(self, path):
+        super().__init__()
+        self._layout = QtWidgets.QVBoxLayout()
+        label = get_disk_name(path)
+        self.label = QtWidgets.QLabel(label)
+
+        self.bar = QtWidgets.QProgressBar()
+        self.bar.setValue(50)
+
+        self._layout.addWidget(self.label)
+        self._layout.addWidget(self.bar)
+
+        self.setLayout(self._layout)
 
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+
+        self.widget = QtWidgets.QWidget()
+        self._layout = QtWidgets.QVBoxLayout()
+        self.disks = []
+        self.disks.append(Disk("E"))
+
+        for d in self.disks:
+            self._layout.addWidget(d)
+        
+        self.widget.setLayout(self._layout)
+        self.setCentralWidget(self.widget)
 
 
 if __name__ == "__main__":
