621b3a43961ac4c933b7cc2afd6de1d6d0e12cc4
Maple <wang-jiaxun@outlook.com>
2024-10-24 15:29:56 +0800

    reformat, finish gui

--- disks.py
+++ disks.py
@@ -3,26 +3,32 @@ from pathlib import Path
 import colorama
 
 from colorama import init
+
 init(autoreset=True)
 
-disk = 'A'
-disks = []
-for i in range(26):
-    disk = chr(ord('A') + i)
-    path = disk + ":/"
-    if Path(path).exists():
-        disks.append(path)
+
+def get_all_disks():
+    disk = "A"
+    disks = []
+    for i in range(26):
+        disk = chr(ord("A") + i)
+        path = disk + ":/"
+        if Path(path).exists():
+            disks.append(path)
+    return disks
 
 
-for d in disks:
-    usage = shutil.disk_usage(d)
-    print(d, end= ' ')
-    percent = usage.used / usage.total * 10
-    if percent > 9:
-        print(colorama.Fore.RED)
-    percent = int(percent)
-    print('[' + '=' * percent + ' '* (10-percent) + "]", end= " ")
+if __name__ == "__main__":
+    disks = get_all_disks()
+    for d in disks:
+        usage = shutil.disk_usage(d)
+        print(d, end=" ")
+        percent = usage.used / usage.total * 10
+        if percent > 9:
+            print(colorama.Fore.RED)
+        percent = int(percent)
+        print("[" + "=" * percent + " " * (10 - percent) + "]", end=" ")
 
-    free_gigabytes = usage.free/1024 /1024/1024
-    percent = usage.free / usage.total * 100
-    print(colorama.Fore.GREEN + f"free: {free_gigabytes:.1f} G ({percent:.1f}%)")
+        free_gigabytes = usage.free / 1024 / 1024 / 1024
+        percent = usage.free / usage.total * 100
+        print(colorama.Fore.GREEN + f"free: {free_gigabytes:.1f} G ({percent:.1f}%)")
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from PySide6 import QtWidgets
 import win32api
+import shutil
+from disks import get_all_disks
 
 
 def get_disk_name(path):
-    return win32api.GetVolumeInformation(f"{path}:\\")[0]
+    return win32api.GetVolumeInformation(f"{path[0]}:\\")[0]
+
+def get_usage(path):
+    stat = shutil.disk_usage(path)
+    return stat.free/1024**3, stat.used/1024**3, stat.total/1024**3
 
 
 class Disk(QtWidgets.QWidget):
@@ -14,10 +20,16 @@ class Disk(QtWidgets.QWidget):
         self.label = QtWidgets.QLabel(label)
 
         self.bar = QtWidgets.QProgressBar()
-        self.bar.setValue(50)
+
+        free, used, total = get_usage(path)
+        self.bar.setValue(int(used/total*100))
+
+        label2 = f"{free:.1f} free of {int(total)} GB"
+        self.info = QtWidgets.QLabel(label2)
 
         self._layout.addWidget(self.label)
         self._layout.addWidget(self.bar)
+        self._layout.addWidget(self.info)
 
         self.setLayout(self._layout)
 
@@ -25,15 +37,17 @@ class Disk(QtWidgets.QWidget):
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Disk Usage")
 
         self.widget = QtWidgets.QWidget()
         self._layout = QtWidgets.QVBoxLayout()
         self.disks = []
-        self.disks.append(Disk("E"))
+        for d in get_all_disks():
+            self.disks.append(Disk(d))
 
         for d in self.disks:
             self._layout.addWidget(d)
-        
+
         self.widget.setLayout(self._layout)
         self.setCentralWidget(self.widget)
 
