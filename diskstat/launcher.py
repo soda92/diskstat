bc067a1b226e00898e94f517ed6446e8ed3fdaa3
sodacris <wjxa20152015@gmail.com>
2025-01-12 15:41:28 +0800

    simplify commadline args

--- diskstat/launcher.py
+++ diskstat/launcher.py
@@ -8,31 +8,21 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "-e", "--enable", action="store_true", default=False, help="enable auto start"
-    )
-    parser.add_argument(
         "-d", "--disable", action="store_true", default=False, help="disable auto start"
     )
     parser.add_argument(
         "-o", "--open", action="store_true", default=False, help="open startup folder"
     )
 
-    parser.add_argument(
-        "-x", "--exec", action="store_true", default=True, help="execute now"
-    )
-
     args = parser.parse_args()
 
-    if args.enable:
-        autostart.enable()
     if args.disable:
         autostart.disable()
 
     if args.open:
         autostart.open_start_folder()
 
-    if args.exec:
-        autostart.exec()
+    autostart.exec()
 
 
 if __name__ == "__main__":
