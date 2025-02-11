c1d6c2f24bbef6a1490686331a6c620a0ba285dd
sodacris <wjxa20152015@gmail.com>
2025-02-11 15:49:58 +0800

    add disks

--- diskstat/program_window.go
+++ diskstat/program_window.go
@@ -1,23 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
+	"os"
 )
+
+func pathExists(path string) bool {
+	_, err := os.Stat(path)
+	return err == nil
+}
+
+func get_disks() []string {
+	x := []string{}
+	for char := 'a'; char <= 'z'; char++ {
+		path := fmt.Sprintf("%c%s", char, ":\\")
+		if pathExists(path) {
+			x = append(x, path)
+		}
+	}
+	return x
+}
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Disk Usage")
 
 	w.Resize(fyne.NewSize(585, 444))
-
-	objects := []widget.Label{}
-	objects = append(objects, *widget.NewLabel("aa"))
 	x := container.New(layout.NewVBoxLayout())
-	x.Add(&objects[0])
+
+	disks := get_disks()
+	for _, v := range disks {
+		x.Add(widget.NewLabel(v))
+	}
 
 	w.SetContent(x)
 	w.ShowAndRun()
